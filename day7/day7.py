40cd0bb4aa6b2985d80febf5adc114d6183c3620
jklypchak13 <jarod.klypchak@gmail.com>
2023-12-08 00:10:12 -0600

    Refactor day 7

--- day7/day7.py
+++ day7/day7.py
@@ -1,73 +1,56 @@
 import sys
 
 
-def get_cards():
-    cards = [f'{i}' for i in range(10)]
-    cards.extend(['T', 'J', 'Q', 'K', 'A'])
-    return cards
-
-
-def get_part_two_cards():
+def get_cards_jokers():
     cards = ['J']
     cards.extend([f'{i}' for i in range(10)])
     cards.extend(['T', 'Q', 'K', 'A'])
-    return cards
+    return {card: i for i, card in enumerate(cards)}
 
 
-def score_hand(hand, jokers=False):
-    counts = {}
-    for char in hand:
-        if char not in counts:
-            counts[char] = 0
-        counts[char] += 1
-    score = 0
+def get_cards():
+    cards = [f'{i}' for i in range(10)]
+    cards.extend(['T', 'J', 'Q', 'K', 'A'])
+    return {card: i for i, card in enumerate(cards)}
+
+
+def get_rank(hand, jokers):
+    counts = {c: hand.count(c) for c in set(hand)}
 
     joker_count = 0
     if jokers and 'J' in counts.keys() and len(counts.keys()) > 1:
         joker_count = counts['J']
         counts['J'] = 0
-
     values = sorted(counts.values())
     values[len(values) - 1] += joker_count
-    for count in values:
-        score += count ** 2
+
+    if 5 in values:
+        return 6
+    if 4 in values:
+        return 5
+    if 3 in values and 2 in values:
+        return 4
+    if 3 in values:
+        return 3
+    if values.count(2) == 2:
+        return 2
+    if 2 in values:
+        return 1
+    return 0
+
+
+def score_hand(hand, jokers=False):
+    cards = get_cards_jokers() if jokers else get_cards()
+    score = get_rank(hand, jokers)
+    for card in hand:
+        score = score * len(cards) + cards[card]
     return score
 
 
-def load_hands(lines, jokers=False):
-    result = {}
-    for line in lines:
-        hand = line.split(' ')[0]
-        bet = line.split(' ')[1]
-        score = 0
-        if jokers:
-            score = score_hand(hand, jokers)
-        else:
-            score = score_hand(hand, jokers)
-        if score not in result:
-            result[score] = []
-        result[score].append((hand, bet))
-    return result
-
-
-def get_total(hands, cards):
-    # Comparator for sorting cards
-    def hand_key(x):
-        res = 0
-        for c in x[0]:
-            res += res * len(cards) + cards.index(c)
-        return res
-
-    total_hands = 1
+def get_total(hands, jokers=False):
     score = 0
-
-    for base in sorted(hands.keys()):
-        values = hands[base]
-        while len(values):
-            current = min(values, key=hand_key)
-            values.remove(current)
-            score += total_hands * int(current[1])
-            total_hands += 1
+    for i, hand in enumerate(sorted(hands, key=lambda hand: score_hand(hand[0], jokers))):
+        score += (i + 1) * hand[1]
     return score
 
 
@@ -77,12 +60,14 @@ if __name__ == '__main__':
         sys.exit(-1)
 
     # Get Data
-    data = ''
+    lines = []
     with open(sys.argv[1], 'r') as fp:
-        data = fp.read()
-    lines = data.split('\n')
+        lines = fp.read().split('\n')
+
+    hands = [(line.split(' ')[0], int(line.split(' ')[1])) for line in lines]
 
     p1_cards = get_cards()
-    p2_cards = get_part_two_cards()
-    print(f'Part 1 Answer: {get_total(load_hands(lines, jokers=False), p1_cards)}')
-    print(f'Part 2 Answer: {get_total(load_hands(lines, jokers=True), p2_cards)}')
+    p2_cards = get_cards_jokers()
+
+    print(f'Part One Answer: {get_total(hands)}')
+    print(f'Part Two Answer: {get_total(hands, jokers=True)}')
