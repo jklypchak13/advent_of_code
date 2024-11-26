b13adb72e96d999a6cef53c568d2ebfa9545a8ed
jklypchak13 <jarod.klypchak@yahoo.com>
2024-11-26 15:18:53 -0600

    Add skeleton for 2024 in typescript

---
+++ 2024/day8/day8.ts
@@ -0,0 +1,13 @@
+import {readFileSync} from "fs";
+import {Result} from "../shared/types.js"
+
+const INPUT_DATA = "day8/input.txt";
+export function day8(){
+    let res: Result = {p1: "", p2: ""};
+    const input = readFileSync(INPUT_DATA, {encoding: "utf-8"});
+    res.p1 = "0";
+    res.p2 = "0";
+    return res;
+}
+
+day8();
---
+++ 2024/shared/types.ts
@@ -0,0 +1,5 @@
+
+export interface Result{
+    p1: String;
+    p2: String;
+}
