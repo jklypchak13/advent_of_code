5b4d5d1bd3169a68a97828a78d1f6b0480cd521d
jklypchak13 <jarod.klypchak@yahoo.com>
2024-12-03 19:44:49 -0600

    Add day 2 and day 3 solutions

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -5,9 +5,54 @@ INPUT_DATA = "day2/input.txt"
 SAMPLE_DATA = "day2/sample.txt"
 
 
+def parse_report(line: str) -> list[int]:
+    return [int(val) for val in line.split(' ')]
+
+
+def is_safe(report: list[int]) -> bool:
+    sign = (report[0] - report[1]) >= 0
+    for i in range(len(report) - 1):
+        diff = report[i] - report[i + 1]
+        # Check if the difference is in the acceptable range, and that we are still matching the direction of change
+        if abs(diff) < 1 or abs(diff) > 3 or (sign != (diff >= 0)):
+            return False
+    return True
+
+
+def count_safe(lines: list[str]) -> bool:
+    count = 0
+    for line in lines:
+        report = parse_report(line)
+        if is_safe(report):
+            count += 1
+    return count
+
+
+def safe_with_error(report: list[int]) -> bool:
+    for i in range(len(report)):
+        # Try to remove the ith element
+        temp = report[0:i]
+        temp.extend(report[i + 1:])
+        if is_safe(temp):
+            return True
+    return False
+
+
+def count_safe_with_error(lines: list[str]) -> bool:
+    count = 0
+    for line in lines:
+        report = parse_report(line)
+        if is_safe(report) or safe_with_error(report):
+            count += 1
+    return count
+
+
 def day2() -> Result:
     res = Result()
     input = get_input(INPUT_DATA)
+
+    res.p1 = count_safe(input)
+    res.p2 = count_safe_with_error(input)
     return res
 
 
--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -1,13 +1,34 @@
 from shared.types import Result
 from shared.input import get_input
+import re
 
 INPUT_DATA = "day3/input.txt"
 SAMPLE_DATA = "day3/sample.txt"
 
 
+def process_instructions(data, regex: re.Pattern):
+    total = 0
+    active = True
+    hits = regex.findall(data)
+    for hit in hits:
+        if hit == 'do()':
+            active = True
+        elif hit == "don't()":
+            active = False
+        elif active:
+            digits = hit[4:-1].split(',')
+            total += int(digits[0]) * int(digits[1])
+    return total
+
+
 def day3() -> Result:
+    MULT_REGEX = 'mul\([0-9]{1,3},[0-9]{1,3}\)'
+    DO_REGEX = 'do\(\)'
+    DONT_REGEX = "don't\(\)"
     res = Result()
-    input = get_input(INPUT_DATA)
+    input = ''.join(get_input(INPUT_DATA))
+    res.p1 = process_instructions(input, re.compile(f'{MULT_REGEX}'))
+    res.p2 = process_instructions(input, re.compile(f'({MULT_REGEX}|{DO_REGEX}|{DONT_REGEX})'))
     return res
 
 
