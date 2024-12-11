08d184868ccb712a9c55d73ed6c1a7d77be68d3c
jklypchak13 <jarod.klypchak@yahoo.com>
2024-12-11 00:33:57 -0600

    Add Day 11 Solution

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,13 +1,51 @@
 from shared.types import Result
 from shared.input import get_input
+from collections import defaultdict
+from functools import cache
 
 INPUT_DATA = "day11/input.txt"
 SAMPLE_DATA = "day11/sample.txt"
 
+StoneCounts = defaultdict[str, int]
+
+
+@cache
+def transform_stone(stone: str) -> tuple[str]:
+    if stone == '0':
+        return ('1',)
+    if len(stone) % 2 == 0:
+        return (str(int(stone[0:len(stone) // 2])), str(int(stone[len(stone) // 2:])))
+    return (str(int(stone) * 2024),)
+
+
+def step_stones(stones: StoneCounts) -> StoneCounts:
+    new_stones = defaultdict(int)
+    for stone in stones:
+        count = stones[stone]
+        transformed = transform_stone(stone)
+
+        # For each new stone, we will get one for each of those values in our original list
+        for new_stone in transformed:
+            new_stones[new_stone] += count
+    return new_stones
+
+
+def take_steps(stones: StoneCounts, step_count: int) -> int:
+    for _ in range(step_count):
+        stones = step_stones(stones)
+    return sum(stones.values())
+
 
 def day11() -> Result:
     res = Result()
-    input = get_input(INPUT_DATA)
+    input = get_input(INPUT_DATA)[0].split(' ')
+
+    # Store the number of each stone that exists
+    stones = defaultdict(int)
+    for stone in input:
+        stones[stone] += 1
+    res.p1 = take_steps(stones, 25)
+    res.p2 = take_steps(stones, 75)
     return res
 
 
