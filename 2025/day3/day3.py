fbd33d8a1868acf1ea27bf8cf72a7df8427d5588
jklypchak13 <jarod.klypchak@yahoo.com>
2025-12-02 23:36:46 -0600

    Add day 3 solution

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -4,11 +4,36 @@ from aoc_utils.input import get_input
 INPUT_DATA = "day3/input.txt"
 SAMPLE_DATA = "day3/sample.txt"
 
+def find_highest(bank, start, end):
+    # Scan backwards, finding the highest value that's the furthest to the left
+    best = int(bank[end])
+    index = end
+    for j in range(end-1, start-1, -1):
+        current_digit = int(bank[j])
+        # Equality, because we want the furthest left max value
+        if current_digit >= best:
+            best = current_digit
+            index = j
+    return best, index
+
+def get_joltage(bank, count):
+    joltage = 0
+    left = 0
+    for i in range(count):
+        right = len(bank) - (count - i)
+        best, index = find_highest(bank, left, right)
+        joltage = joltage * 10 + best
+        left = index + 1
+    return joltage
+
 def day3() -> Result:
     res = Result()
-    input = get_input(INPUT_DATA)
-    res.p1 = '0'
-    res.p2 = '0'
+    input_lines = get_input(INPUT_DATA)
+    res.p1 = 0
+    res.p2 = 0
+    for line in input_lines:
+        res.p1 += get_joltage(line, 2)
+        res.p2 += get_joltage(line, 12)
     return res
 
 
