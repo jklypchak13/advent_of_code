9888efd3c10f4a3ce37e00aa96351016246c815d
jklypchak13 <jarod.klypchak@yahoo.com>
2025-12-06 23:38:31 -0600

    Add day 7 solution

--- 2025/day7/day7.py
+++ 2025/day7/day7.py
@@ -1,14 +1,34 @@
 from aoc_utils.types import Result
 from aoc_utils.input import get_input
+from collections import defaultdict
 
 INPUT_DATA = "day7/input.txt"
 SAMPLE_DATA = "day7/sample.txt"
 
+def count_timelines(lines):
+    start = lines[0].index('S')
+    splits = 0
+    prev_active_particles = defaultdict(int)
+    prev_active_particles[start] = 1
+    for row in lines[1:]:
+        new_active_particles = defaultdict(int)
+        for i, current in enumerate(row):
+            if i not in prev_active_particles:
+                continue
+            particle_count = prev_active_particles[i]
+            if current == '.':
+                new_active_particles[i] += particle_count
+            elif current == '^':
+                splits += 1
+                new_active_particles[i-1] += particle_count
+                new_active_particles[i+1] += particle_count
+        prev_active_particles = new_active_particles
+    return splits, sum(prev_active_particles.values())
+
 def day7() -> Result:
     res = Result()
-    input = get_input(INPUT_DATA)
-    res.p1 = '0'
-    res.p2 = '0'
+    lines = get_input(INPUT_DATA)
+    res.p1, res.p2 = count_timelines(lines)
     return res
 
 
